710a55a524113c2755f673e4a1d2a462f7160c98
V G <vladislav.goncharenko@phystech.edu>
2020-12-23 20:34:59 +0300

    added prediction conversion to scripted model

--- yolo_scripted.py
+++ yolo_scripted.py
@@ -3,10 +3,12 @@ from pathlib import Path
 import torch
 from torch import Tensor, device as Device
 
+from models.experimental import attempt_load
+
 File = Path
 
 
-class DetectionFacade(torch.nn.Module):
+class YoloFacade(torch.nn.Module):
     '''Exposes traced NN to final inference via TorchScript.
 
     Loads the traced trained model and perform `forward` method of it
@@ -15,17 +17,20 @@ class DetectionFacade(torch.nn.Module):
 
     def __init__(
         self,
-        yolo_path: File,
+        traced_path: File,
         stride: Tensor,
         anchor_grid: Tensor,
-        device: Device,
         conf_thres: float = 0.25,
         iou_thres: float = 0.45,
         agnostic_nms: bool = True,
+        device: Device,
     ):
         '''
         Args:
-            yolo_path: path to traced model
+            traced_path: path to traced model
+            stride, anchor_grid: params from full fledged model to convert bboxes
+                to original scale
+            conf_thres, iou_thres, agnostic_nms: params of NMS
             device: cuda or cpu
         '''
         super().__init__()
@@ -58,31 +63,38 @@ class DetectionFacade(torch.nn.Module):
         res = []
 
         for i in range(len(raw)):
-            bs, na, no, ny, nx = raw[i].shape[2:4] # wrong
+            bs, na, ny, nx, no = raw[i].shape
             if self._grid[i].shape[2:4] != raw[i].shape[2:4]:
                 self._grid[i] = self._make_grid(nx, ny)
 
             y = raw[i].sigmoid()
             y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self._grid[i]) * self.stride[i]  # xy
             y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-            res.append(y.view(bs, -1, self.no))
+            res.append(y.view(bs, -1, no))
 
-        return torch.tensor(()).cpu()
+        return torch.cat(res, 1)
+        # return torch.tensor(()).cpu()
 
     def _make_grid(self, nx: int = 20, ny: int = 20):
         yv, xv = torch.meshgrid((torch.arange(ny, device=self.device), torch.arange(nx, device=self.device)))
         return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
 
     @classmethod
-    def script(cls, traced_path: File, scripted_path: File, device: Device):
+    def script(cls yolo_path: File, traced_path: File, scripted_path: File, device: Device):
         '''Saves scripted version of Facade to scripted_path
 
         Args:
+            yolo_path: original model dump (needed to get parameters which is not traced)
             traced_path: tha path to traced trained model
             scripted_path: the path for scripted model
             device: the device
         '''
-        facade = cls(traced_path, device)
+        model = attempt_load(yolo_path.as_posix())
+        detect = model.model[-1]
+        stride = detect.stride.clone().detach()
+        anchor_grid = detect.anchor_grid.clone().detach()
+
+        facade = cls(traced_path, stride, anchor_grid, device=device)
         scripted = torch.jit.script(facade)
         scripted.save(scripted_path.as_posix())
 
